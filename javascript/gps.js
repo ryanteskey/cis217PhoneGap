94398ccbc93f553be50bee9a425629ce4b5c62cb
Ryan Teskey <ryanteskey@gmail.com>
2013-06-10 23:56:24 -0700

    getting phoneGap Geolocation to work with my application.

--- javascript/gps.js
+++ javascript/gps.js
@@ -1,45 +1,46 @@
-// Put your custom code here
 jQuery(function(){
+// Wait for Cordova to load
+    //
+    var destination_cords = '47.7459739, -117.404516';
+    document.addEventListener("deviceready", onDeviceReady, false);
 
-    navigator.geolocation.getCurrentPosition(function(pos){
-        var lat = pos.coords.latitude;
-        var lon = pos.coords.longitude;
-//        var map = '<iframe width="300px" height="300px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=d&amp;source=s_d&amp;saddr='
-//            + lat +',' + lon +
-//            '&amp;daddr=Shakey\'s+Pizza+Parlor,+9602+North+Newport+Highway,+Spokane,+WA+99218&amp;output=embed&amp;z=11"></iframe><br /><small><a href="https://maps.google.com/maps?f=d&amp;source=embed&amp;saddr='
-//            + lat +',' + lon +
-//            '&amp;daddr=Shakey\'s+Pizza+Parlor,+9602+North+Newport+Highway,+Spokane,+WA+99218&amp;hl=en&amp;aq=&amp;sll='
-//            + lat +',' + lon +
-//            '&amp;spn='
-//            + lat +',' + lon +
-//            '" style="color:#0000FF;text-align:left">View Larger Map</a></small>';
-        var destination = "Shakey's+Pizza+Parlor,+9602+N+Newport+Hwy,+Spokane,+WA&amp;";
-        var destination_cords = '47.7459739, -117.404516';
+    // Cordova is ready
+    //
+    function onDeviceReady() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
 
+    // onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
         var map = '<iframe ' +
-                            'width="300" ' +
-                            'height="300" ' +
-                            'frameborder="0" ' +
-                            'scrolling="no" ' +
-                            'marginheight="0" ' +
-                            'marginwidth="0" ' +
-                            'src="https://maps.google.com/maps?' +
-                            'daddr='+destination_cords +
-                            '&amp;saddr=' + lat +',' + lon + '&amp;' +
-                            'output=embed&amp;' +
-                            'z=11"></iframe>' +
-                            '<br /><small><a ' +
+            'width="300" ' +
+            'height="300" ' +
+            'frameborder="0" ' +
+            'scrolling="no" ' +
+            'marginheight="0" ' +
+            'marginwidth="0" ' +
+            'src="https://maps.google.com/maps?' +
+            'daddr='+destination_cords +
+            '&amp;saddr=' + lat +',' + lon + '&amp;' +
+            'output=embed&amp;' +
+            'z=11"></iframe>' +
+            '<br /><small><a ' +
             'href="https://maps.google.com/maps?' +
             'f=d&amp;' +
             'source=embed&amp;' +
             'saddr=' + lat +',' + lon + '&amp;' +
             'daddr=' + destination_cords + '&amp;' +
             ' style="color:#0000FF;text-align:left">View Larger Map</a></small>';
-
-
-
-
-
         $("#directionMap").html(map);
-    })
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+            'message: ' + error.message + '\n');
+    }
 })
